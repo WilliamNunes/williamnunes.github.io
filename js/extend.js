28752eb18178813debbde7a1c79be73bf9c7445b
William Nunes <williamnunes11@gmail.com>
2018-04-27 07:32:17 -0300

    Page
    Upload my page

---
+++ js/extend.js
@@ -0,0 +1,38 @@
+$(function() {
+  $('.chart').easyPieChart({
+    scaleColor: "transparent",
+    barColor: '#975a5f',
+    trackColor:	"#212121",
+    lineWidth: 7,
+    size: 90,
+    animate: 1000
+  });
+});
+
+$(function() {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 650 || screen.width < 992) {
+      $('#showme').slideUp(1000);
+    } else {
+      $("#showme").slideDown(1000);
+    }
+  });
+});
+
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+$('.img-resizing').on( "click", function() {
+    $("#showme").slideUp(1000);
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+});
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
